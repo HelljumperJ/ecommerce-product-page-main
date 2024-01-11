cdbc09883a8ca2d03ea2c6c79b3e34ce2415cde0
HelljumperJ <jefrey.poveda17@gmail.com>
2024-01-11 15:33:14 -0500

    disend

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector("#btnSumar").addEventListener("click",sumarUno);
+document.querySelector("#btnRestar").addEventListener("click",restarUno);
+let contador=0
+
+function sumarUno(){
+    contador=contador+1
+    document.querySelector("#msgContador").innerHTML=contador
+}
+
+function restarUno(){
+    contador=contador-1
+    document.querySelector("#msgContador").innerHTML=contador
+}
+
+if (contador=0){
+    restarUno:disabled
+
+}
+
